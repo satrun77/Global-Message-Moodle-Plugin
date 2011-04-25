2a0f59174bb36a79426b9a41a930f61572701403
Mohamed Alsharaf <mohamed.alsharaf@gmail.com>
2011-04-26 01:59:54 +1200

    Increase version number and update readme.txt

--- lib/base.php
+++ lib/base.php
@@ -8,12 +8,12 @@ define('INTERNAL_ACCESS', 1);
  * @subpackage globalmessage
  * @copyright  2011 Mohamed Alsharaf
  * @author     Mohamed Alsharaf (mohamed.alsharaf@gmail.com)
- * @version    1.1.0
+ * @version    1.1.1
  * @license    http://www.gnu.org/copyleft/gpl.html
  */
 class moo_globalmessage
 {
-    private $version = '1.1.0';
+    private $version = '1.1.1';
     protected $user;
     protected $config;
     protected $course;
