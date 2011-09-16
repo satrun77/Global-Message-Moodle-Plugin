aaa7b67aca63a8ceb9294d4c62d2efd907016d93
Mohamed Alsharaf <mohamed.alsharaf@gmail.com>
2011-09-16 19:51:36 +1200

    several minnor bug fixes

--- settings.php
+++ settings.php
@@ -4,7 +4,4 @@ defined('MOODLE_INTERNAL') || die;
 if ($hassiteconfig && isset($ADMIN)) {
     include_once realpath(dirname(__FILE__)) . '/lib/base.php';
     moo_globalmessage::set_adminsettings($ADMIN);
-//    $ADMIN->add('root', new admin_externalpage('qeupgradehelper',
-//            get_string('pluginname', 'local_qeupgradehelper'),
-//            new moodle_url('/local/qeupgradehelper/index.php')));
 }
--- version.php
+++ version.php
@@ -1,4 +1,4 @@
 <?php
 
 $plugin->version  = 2010022403;   // The (date) version of this plugin
-$plugin->requires = 2010021900;   // Requires this Moodle version
+$plugin->requires = 2010080300;   // Requires this Moodle version
